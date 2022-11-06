e1a10b873332fc40afb4f8ace7750a4e353deb6c
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-06 15:39:58 +0100

    ikoner klara med funktion

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 
-    facebookIcon.style.opacity="0";
-    facebookText.style.color="#909090";
-    instagramIcon.style.opacity="1";
-    instagramText.style.color="black";
-    instagramIcon.style.opacity="0";
-    instagramText.style.color="#909090";
-    facebookIcon.style.opacity="1";
-    facebookText.style.color="black";
+   
+   
 let icons= {
     instagram:[document.getElementById("insta-icon"),document.getElementById("insta-text")],
     facebook:[document.getElementById("facebook-icon"),document.getElementById("facebook-text")],
     mail:[document.getElementById("mail"),document.getElementById("mail-title")],
     phone:[]
-
-
 }
-
-    mailAdress.style.color="black";
 function displayInsta(){
     icons.instagram[0].style.opacity="1";
-    icons.instagram[1]
-
+    icons.instagram[1].style.color="black";
 }
 function hideInsta(){
-
-
-   
+    icons.instagram[1].style.color="#909090";
+    icons.instagram[0].style.opacity="0";
 }
 function displayFacebook(){
    
-  
+    icons.facebook[0].style.opacity="1";
+    icons.facebook[1].style.color="black";
 }
 function hideFacebook(){
-   
-   
+    icons.facebook[1].style.color="#909090";
+    icons.facebook[0].style.opacity="0";
 }
 function blackMail(){
-
+   icons.mail[1].style.color="black";
+}
+function greyMail(){
+    icons.mail[1].style.color="#909090";
+   
 }
