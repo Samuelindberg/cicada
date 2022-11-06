24392fbbf5d4165bb00fc567debf8eada2946f3a
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-06 14:41:29 +0100

    script för hover att visa insta ikon mm

---
+++ script.js
@@ -0,0 +1,40 @@
+
+    facebookIcon.style.opacity="0";
+    facebookText.style.color="#909090";
+    instagramIcon.style.opacity="1";
+    instagramText.style.color="black";
+    instagramIcon.style.opacity="0";
+    instagramText.style.color="#909090";
+    facebookIcon.style.opacity="1";
+    facebookText.style.color="black";
+let icons= {
+    instagram:[document.getElementById("insta-icon"),document.getElementById("insta-text")],
+    facebook:[document.getElementById("facebook-icon"),document.getElementById("facebook-text")],
+    mail:[document.getElementById("mail"),document.getElementById("mail-title")],
+    phone:[]
+
+
+}
+
+    mailAdress.style.color="black";
+function displayInsta(){
+    icons.instagram[0].style.opacity="1";
+    icons.instagram[1]
+
+}
+function hideInsta(){
+
+
+   
+}
+function displayFacebook(){
+   
+  
+}
+function hideFacebook(){
+   
+   
+}
+function blackMail(){
+
+}
