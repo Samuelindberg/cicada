153f4e889aa9674dc62bb09ba3e270b08a4dab57
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-08 15:32:45 +0100

    mediascreen på förstasidan och början av kontakt

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let icons= {
 }
 let hamburgermenu={
     hamburgerIcon: document.getElementById("hamburger-icon"),
-    hamburgerBars: [document.getElementById("bar1"),document.getElementById("bar2"),document.getElementById("bar3")]
+    hamburgerBars: [document.getElementById("bar1"),document.getElementById("bar2"),document.getElementById("bar3")],
+    menu: document.getElementById("hamburger-links"),
+    menuOpen:false
 }
-
 icons.instagram[1].addEventListener("mouseenter",function(){
     icons.instagram[0].style.opacity="1";
     icons.instagram[1].style.color="black";
@@ -34,6 +35,31 @@ icons.mail[1].addEventListener("mouseleave",function(){
 });
 
 
-hamburgermenu.hamburgerIcon.addEventListener("click",function(){
+hamburgermenu.hamburgerIcon.addEventListener("mouseenter",function(){
     alert("funkar!");
 });
+
+function menu(){
+    if(hamburgermenu.menuOpen){
+        hamburgermenu.hamburgerBars[0].style.transform="rotate(45deg) translateY(9.5px)";
+        hamburgermenu.hamburgerBars[0].style.backgroundColor="white";
+        hamburgermenu.hamburgerBars[1].style.opacity="0";
+        hamburgermenu.hamburgerBars[1].style.backgroundColor="white";
+        hamburgermenu.hamburgerBars[2].style.transform="rotate(-45deg) translate(2px, -11px)";
+        hamburgermenu.hamburgerBars[2].style.backgroundColor="white";
+        hamburgermenu.menu.style.transform="translateX(0)";
+        hamburgermenu.menuOpen=false;
+    }
+    else{
+        hamburgermenu.menu.style.transform="translateX(-100vw)";
+        hamburgermenu.hamburgerBars[0].style.transform="rotate(0)";
+        hamburgermenu.hamburgerBars[0].style.backgroundColor="#8ba046";
+        hamburgermenu.hamburgerBars[1].style.opacity="1";
+        hamburgermenu.hamburgerBars[1].style.backgroundColor="#8ba046";
+        hamburgermenu.hamburgerBars[2].style.transform="rotate(0)";
+        hamburgermenu.hamburgerBars[2].style.backgroundColor="#8ba046";
+        hamburgermenu.menuOpen=true;
+
+    }
+
+}
